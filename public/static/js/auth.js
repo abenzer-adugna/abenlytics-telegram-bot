5c1fc90cc3a23983443a4619b1984c8641cac3f7
abenzer-adugna <86248246+abenzer-adugna@users.noreply.github.com>
2025-06-12 23:45:40 +0300

    Update auth.js

--- public/static/js/auth.js
+++ public/static/js/auth.js
@@ -1,17 +1,17 @@
-document.getElementById('loginForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const response = await fetch('/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            username: document.getElementById('username').value,
-            password: document.getElementById('password').value
-        })
-    });
+//document.getElementById('loginForm').addEventListener('submit', async (e) => {
+  //  e.preventDefault();
+  //  const response = await fetch('/login', {
+     //   method: 'POST',
+       // headers: { 'Content-Type': 'application/json' },
+       // body: JSON.stringify({
+       //     username: document.getElementById('username').value,
+        //    password: document.getElementById('password').value
+     //   })
+ //   });
     
-    if (response.ok) {
-        window.location.href = '/';
-    } else {
-        document.getElementById('errorMessage').textContent = 'Invalid credentials';
-    }
-});
+   // if (response.ok) {
+      //  window.location.href = '/';
+ //   } else {
+    //    document.getElementById('errorMessage').textContent = 'Invalid credentials';
+//    }
+//});
